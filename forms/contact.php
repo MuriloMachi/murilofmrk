672ff2d01e7d0dc48bc14ba115883a1728d2a2bd
MuriloMachi <143286318+MuriloMachi@users.noreply.github.com>
2023-10-03 22:51:26 -0300

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,23 @@
+<?php
+  $receiving_email_address = 'mferreiramachi@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['Seu Nome'];
+  $contact->from_email = $_POST['Email'];
+  $contact->subject = $_POST['Assunto'];
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Menssagem', 10);
+
+  echo $contact->get();
+?>
